cbbf182a52743276d8fd3472099e565f777eabe8
Martin Wiesemann <martin@martinwiesemann.de>
2023-08-11 16:40:02 +0200

    removed the scripts for seafile

--- bin/delete_old_backups.sh
+++ bin/delete_old_backups.sh
@@ -5,7 +5,6 @@
 
 BASEDIR="backups"
 
-find $BASEDIR -type f -name '*.tar' -mtime +14 -exec bin/seafile_delete_file.sh "{}" \;
 find $BASEDIR -type f -name '*.tar' -mtime +14 -delete
 
 # end of file
